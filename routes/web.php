6592da05df7d9d1f59844b705e0f60fece93309d
guramin01 <48230672+guramin01@users.noreply.github.com>
2019-10-05 16:56:55 +0700

    Responsive Tamplate Blade Laravel
    Penerapan Frontend ke Tamplate Blade Laravel

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,34 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'IndexController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('halo', function () {
-	return "Halo ini gede store kita ";
-});
 
-Route::get('test', function(){
-	return view('test');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//Public Front ENd
+Route::get('/blog', 'BlogController@blog');
+Route::get('/blog/tentang', 'BlogController@tentang');
+Route::get('/blog/kontak', 'BlogController@kontak');
+
+// Route::get('halo', function () {
+// 	return "Halo ini gede store kita ";
+// });
+
+
+
+// // get data dari form
+// Route::get('/formulir', 'TestController@formulir');
+// Route::post('/formulir/proses', 'TestController@proses');
+// // get data uri
+// Route::get('/test/{uri}','TestController@index');
+// //route irfan controller
+// Route::get('/irfan','IrfanController@index');
+
+//auth
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
