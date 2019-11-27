620671d8bbd3b36c6640cf2c2d94eaf06945f657
Re <sdragneel6661@gmail.com>
2019-11-28 00:00:52 +0700

    [#]transmasuk

--- app/Http/Controllers/TransaksiMasukController.php
+++ app/Http/Controllers/TransaksiMasukController.php
@@ -29,13 +29,48 @@ class TransaksiMasukController extends Controller
             'dtstok' => DB::table('tb_detail_transaksi_masuk')
                             ->join('tb_stok', 'tb_stok.stok_id', '=', 'tb_detail_transaksi_masuk.detiltrans_masuk_idstok')
                             ->join('tb_barang', 'tb_barang.barang_id', '=', 'tb_stok.stok_barang_id')
-                            ->select('tb_barang.barang_id', 'tb_barang.barang_nama', 'tb_stok.stok_ukuran', 'tb_detail_transaksi_masuk.detiltrans_masuk_stok', 'tb_detail_transaksi_masuk.detiltrans_masuk_totalHarga')
+                            ->select('tb_barang.barang_id', 'tb_barang.barang_nama', 'tb_stok.stok_ukuran', 'tb_detail_transaksi_masuk.detiltrans_masuk_stok', 'tb_detail_transaksi_masuk.detiltrans_masuk_totalHarga', 'tb_stok.stok_id', 'tb_detail_transaksi_masuk.detiltrans_masuk_id')
                             ->where('tb_detail_transaksi_masuk.detiltrans_masuk_idtrans', $lastkode)
                             ->get(),
             'lastid' => $lastkode
-            
         ];
         
         return view ('admin/tr_barangMasuk', $data);
+    }
+    
+    public function tambah_detail($id, $idtr) {
+        $stok = DB::table('tb_stok')
+                        ->join('tb_barang', 'tb_barang.barang_id', '=', 'tb_stok.stok_barang_id')
+                        ->select('tb_barang.barang_harga_beli', 'tb_stok.stok_jumlah_stok')
+                        ->where('tb_stok.stok_id', $id)
+                        ->first();
+        $total = $stok->stok_jumlah_stok - 1;
+        DB::table('tb_stok')->where('tb_stok.stok_id', $id)->update(['stok_jumlah_stok' => $total]);
+        
+        DB::table('tb_detail_transaksi_masuk')->insert([
+            'detiltrans_masuk_idtrans' => $idtr,
+            'detiltrans_masuk_idstok' => $id,
+            'detiltrans_masuk_stok' => '1',
+            'detiltrans_masuk_totalHarga' => $stok->barang_harga_beli
+        ]);
+        
+        return redirect('/admin/halaman-transaksi-barang-masuk');
+    }
+    
+    public function delete_detail($id) {
+        $returnstok = DB::table('tb_detail_transaksi_masuk')
+                        ->select('detiltrans_masuk_idstok', 'detiltrans_masuk_stok')
+                        ->where('detiltrans_masuk_id', $id)
+                        ->first();
+        
+        $stok = DB::table('tb_stok')->where('stok_id', $returnstok->detiltrans_masuk_idstok)->select('*')->first();
+        $total = $returnstok->detiltrans_masuk_stok + $stok->stok_jumlah_stok;
+        DB::table('tb_stok')->where('stok_id', $returnstok->detiltrans_masuk_idstok)
+            ->update([
+                'stok_jumlah_stok' => $total
+            ]);
+        
+        DB::table('tb_detail_transaksi_masuk')->where('detiltrans_masuk_id', $id)->delete();
+        return redirect('/admin/halaman-transaksi-barang-masuk');
     }
 }
