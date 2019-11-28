014af693ee2e0ffc7f4cba132407c4edf16fb083
yanuar23 <yanuar.ridwan.h@gmail.com>
2019-11-28 11:25:07 +0700

    database baru

--- DB_GEDE/db_gede.sql
+++ DB_GEDE/db_gede.sql
@@ -2,10 +2,10 @@
 -- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 27 Nov 2019 pada 19.21
--- Versi server: 10.4.6-MariaDB
--- Versi PHP: 7.2.22
+-- Host: localhost
+-- Generation Time: Nov 28, 2019 at 05:24 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.2.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `gede`
+-- Database: `db_gede`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `migrations`
+-- Table structure for table `migrations`
 --
 
 CREATE TABLE `migrations` (
@@ -35,7 +35,7 @@ CREATE TABLE `migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data untuk tabel `migrations`
+-- Dumping data for table `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_admins`
+-- Table structure for table `tb_admins`
 --
 
 CREATE TABLE `tb_admins` (
@@ -56,7 +56,7 @@ CREATE TABLE `tb_admins` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tb_admins`
+-- Dumping data for table `tb_admins`
 --
 
 INSERT INTO `tb_admins` (`admin_id`, `admin_nama`, `admin_email`, `password`) VALUES
@@ -65,7 +65,7 @@ INSERT INTO `tb_admins` (`admin_id`, `admin_nama`, `admin_email`, `password`) VA
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_barang`
+-- Table structure for table `tb_barang`
 --
 
 CREATE TABLE `tb_barang` (
@@ -79,7 +79,7 @@ CREATE TABLE `tb_barang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tb_barang`
+-- Dumping data for table `tb_barang`
 --
 
 INSERT INTO `tb_barang` (`barang_id`, `barang_nama`, `barang_harga_beli`, `barang_harga_jual`, `barang_deskripsi`, `barang_kategori_id`, `barang_gambar`) VALUES
@@ -90,7 +90,7 @@ INSERT INTO `tb_barang` (`barang_id`, `barang_nama`, `barang_harga_beli`, `baran
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_detail_transaksi`
+-- Table structure for table `tb_detail_transaksi`
 --
 
 CREATE TABLE `tb_detail_transaksi` (
@@ -103,22 +103,25 @@ CREATE TABLE `tb_detail_transaksi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tb_detail_transaksi`
+-- Dumping data for table `tb_detail_transaksi`
 --
 
 INSERT INTO `tb_detail_transaksi` (`dt_nomor`, `dt_transaksi_nomor`, `dt_barang_id`, `dt_barang_ukuran`, `dt_jumlah_barang`, `dt_jumlah_harga`) VALUES
 (6, 'TRX-000000001', '5dc00fc76fa15', 'XL', 4, 88000),
 (7, 'TRX-000000001', '5dc00fc76fa15', 'L', 1, 22000),
 (8, 'TRX-000000002', '5dc00fc76fa15', 'XL', 4, 88000),
-(9, 'TRX-000000002', '5dc00fc76fa15', 'L', 1, 22000);
+(9, 'TRX-000000002', '5dc00fc76fa15', 'L', 1, 22000),
+(10, 'TRX-000000003', '5dc02df14b5ff', 'XL', 5, 750000),
+(11, 'TRX-000000003', '5dd8712959982', 'L', 1, 123123123);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_detail_transaksi_masuk`
+-- Table structure for table `tb_detail_transaksi_masuk`
 --
 
 CREATE TABLE `tb_detail_transaksi_masuk` (
+  `detiltrans_masuk_id` int(11) NOT NULL,
   `detiltrans_masuk_idtrans` varchar(13) NOT NULL,
   `detiltrans_masuk_idstok` int(11) NOT NULL,
   `detiltrans_masuk_stok` int(4) NOT NULL,
@@ -126,16 +129,16 @@ CREATE TABLE `tb_detail_transaksi_masuk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tb_detail_transaksi_masuk`
+-- Dumping data for table `tb_detail_transaksi_masuk`
 --
 
-INSERT INTO `tb_detail_transaksi_masuk` (`detiltrans_masuk_idtrans`, `detiltrans_masuk_idstok`, `detiltrans_masuk_stok`, `detiltrans_masuk_totalHarga`) VALUES
-('TRX-000000001', 1, 5, 100000);
+INSERT INTO `tb_detail_transaksi_masuk` (`detiltrans_masuk_id`, `detiltrans_masuk_idtrans`, `detiltrans_masuk_idstok`, `detiltrans_masuk_stok`, `detiltrans_masuk_totalHarga`) VALUES
+(1, 'TRX-000000001', 1, 5, 100000);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_kategori`
+-- Table structure for table `tb_kategori`
 --
 
 CREATE TABLE `tb_kategori` (
@@ -144,7 +147,7 @@ CREATE TABLE `tb_kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tb_kategori`
+-- Dumping data for table `tb_kategori`
 --
 
 INSERT INTO `tb_kategori` (`kategori_id`, `kategori_nama`) VALUES
@@ -153,7 +156,7 @@ INSERT INTO `tb_kategori` (`kategori_id`, `kategori_nama`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_konfirmasi`
+-- Table structure for table `tb_konfirmasi`
 --
 
 CREATE TABLE `tb_konfirmasi` (
@@ -168,7 +171,7 @@ CREATE TABLE `tb_konfirmasi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_member`
+-- Table structure for table `tb_member`
 --
 
 CREATE TABLE `tb_member` (
@@ -180,7 +183,7 @@ CREATE TABLE `tb_member` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_stok`
+-- Table structure for table `tb_stok`
 --
 
 CREATE TABLE `tb_stok` (
@@ -191,7 +194,7 @@ CREATE TABLE `tb_stok` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tb_stok`
+-- Dumping data for table `tb_stok`
 --
 
 INSERT INTO `tb_stok` (`stok_id`, `stok_barang_id`, `stok_ukuran`, `stok_jumlah_stok`) VALUES
@@ -209,7 +212,7 @@ INSERT INTO `tb_stok` (`stok_id`, `stok_barang_id`, `stok_ukuran`, `stok_jumlah_
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_transaksi`
+-- Table structure for table `tb_transaksi`
 --
 
 CREATE TABLE `tb_transaksi` (
@@ -221,17 +224,18 @@ CREATE TABLE `tb_transaksi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tb_transaksi`
+-- Dumping data for table `tb_transaksi`
 --
 
 INSERT INTO `tb_transaksi` (`transaksi_nomor`, `transaksi_member_id`, `transaksi_tanggal`, `transaksi_alamat_pengiriman`, `transaksi_jumlah_uang`) VALUES
 ('TRX-000000001', 0, '2019-11-27', '-', 110000),
-('TRX-000000002', 0, '2019-11-27', '-', 110000);
+('TRX-000000002', 0, '2019-11-27', '-', 110000),
+('TRX-000000003', 0, '2019-11-28', '-', 123873123);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_transaksi_masuk`
+-- Table structure for table `tb_transaksi_masuk`
 --
 
 CREATE TABLE `tb_transaksi_masuk` (
@@ -242,7 +246,7 @@ CREATE TABLE `tb_transaksi_masuk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tb_transaksi_masuk`
+-- Dumping data for table `tb_transaksi_masuk`
 --
 
 INSERT INTO `tb_transaksi_masuk` (`trans_masuk_id`, `trans_masuk_tanggal`, `trans_masuk_suplier`, `trans_masuk_totalharga`) VALUES
@@ -251,7 +255,7 @@ INSERT INTO `tb_transaksi_masuk` (`trans_masuk_id`, `trans_masuk_tanggal`, `tran
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_users`
+-- Table structure for table `tb_users`
 --
 
 CREATE TABLE `tb_users` (
@@ -263,10 +267,17 @@ CREATE TABLE `tb_users` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `tb_users`
+--
+
+INSERT INTO `tb_users` (`idUser`, `namaUser`, `emailUser`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'yanuar', 'yanuar.ridwan.h@gmail.com', '4b3865dc277de5870cce27623f1e20fc', '2019-11-27 21:01:30', NULL);
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tb_wishlists`
+-- Table structure for table `tb_wishlists`
 --
 
 CREATE TABLE `tb_wishlists` (
@@ -282,26 +293,26 @@ CREATE TABLE `tb_wishlists` (
 --
 
 --
--- Indeks untuk tabel `migrations`
+-- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `tb_admins`
+-- Indexes for table `tb_admins`
 --
 ALTER TABLE `tb_admins`
   ADD PRIMARY KEY (`admin_id`);
 
 --
--- Indeks untuk tabel `tb_barang`
+-- Indexes for table `tb_barang`
 --
 ALTER TABLE `tb_barang`
   ADD PRIMARY KEY (`barang_id`),
   ADD KEY `barang_kategori_id` (`barang_kategori_id`);
 
 --
--- Indeks untuk tabel `tb_detail_transaksi`
+-- Indexes for table `tb_detail_transaksi`
 --
 ALTER TABLE `tb_detail_transaksi`
   ADD PRIMARY KEY (`dt_nomor`),
@@ -309,13 +320,19 @@ ALTER TABLE `tb_detail_transaksi`
   ADD KEY `dt_barang_id` (`dt_barang_id`);
 
 --
--- Indeks untuk tabel `tb_kategori`
+-- Indexes for table `tb_detail_transaksi_masuk`
+--
+ALTER TABLE `tb_detail_transaksi_masuk`
+  ADD PRIMARY KEY (`detiltrans_masuk_id`);
+
+--
+-- Indexes for table `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
   ADD PRIMARY KEY (`kategori_id`);
 
 --
--- Indeks untuk tabel `tb_konfirmasi`
+-- Indexes for table `tb_konfirmasi`
 --
 ALTER TABLE `tb_konfirmasi`
   ADD PRIMARY KEY (`konfirmasi_nomor`),
@@ -325,101 +342,107 @@ ALTER TABLE `tb_konfirmasi`
   ADD KEY `konfirmasi_trans_no` (`konfirmasi_trans_no`);
 
 --
--- Indeks untuk tabel `tb_member`
+-- Indexes for table `tb_member`
 --
 ALTER TABLE `tb_member`
   ADD PRIMARY KEY (`member_id`);
 
 --
--- Indeks untuk tabel `tb_stok`
+-- Indexes for table `tb_stok`
 --
 ALTER TABLE `tb_stok`
   ADD PRIMARY KEY (`stok_id`),
   ADD KEY `stok_barang_id` (`stok_barang_id`);
 
 --
--- Indeks untuk tabel `tb_transaksi`
+-- Indexes for table `tb_transaksi`
 --
 ALTER TABLE `tb_transaksi`
   ADD PRIMARY KEY (`transaksi_nomor`),
   ADD KEY `transaksi_member_id` (`transaksi_member_id`);
 
 --
--- Indeks untuk tabel `tb_transaksi_masuk`
+-- Indexes for table `tb_transaksi_masuk`
 --
 ALTER TABLE `tb_transaksi_masuk`
   ADD PRIMARY KEY (`trans_masuk_id`);
 
 --
--- Indeks untuk tabel `tb_users`
+-- Indexes for table `tb_users`
 --
 ALTER TABLE `tb_users`
   ADD PRIMARY KEY (`idUser`);
 
 --
--- Indeks untuk tabel `tb_wishlists`
+-- Indexes for table `tb_wishlists`
 --
 ALTER TABLE `tb_wishlists`
   ADD PRIMARY KEY (`wishlist_id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `migrations`
+-- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_detail_transaksi`
+-- AUTO_INCREMENT for table `tb_detail_transaksi`
 --
 ALTER TABLE `tb_detail_transaksi`
-  MODIFY `dt_nomor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `dt_nomor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_kategori`
+-- AUTO_INCREMENT for table `tb_detail_transaksi_masuk`
+--
+ALTER TABLE `tb_detail_transaksi_masuk`
+  MODIFY `detiltrans_masuk_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
   MODIFY `kategori_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_stok`
+-- AUTO_INCREMENT for table `tb_stok`
 --
 ALTER TABLE `tb_stok`
   MODIFY `stok_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_users`
+-- AUTO_INCREMENT for table `tb_users`
 --
 ALTER TABLE `tb_users`
-  MODIFY `idUser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idUser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `tb_wishlists`
+-- AUTO_INCREMENT for table `tb_wishlists`
 --
 ALTER TABLE `tb_wishlists`
   MODIFY `wishlist_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `tb_barang`
+-- Constraints for table `tb_barang`
 --
 ALTER TABLE `tb_barang`
   ADD CONSTRAINT `tb_barang_ibfk_1` FOREIGN KEY (`barang_kategori_id`) REFERENCES `tb_kategori` (`kategori_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `tb_detail_transaksi`
+-- Constraints for table `tb_detail_transaksi`
 --
 ALTER TABLE `tb_detail_transaksi`
   ADD CONSTRAINT `tb_detail_transaksi_ibfk_2` FOREIGN KEY (`dt_barang_id`) REFERENCES `tb_barang` (`barang_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `tb_konfirmasi`
+-- Constraints for table `tb_konfirmasi`
 --
 ALTER TABLE `tb_konfirmasi`
   ADD CONSTRAINT `tb_konfirmasi_ibfk_1` FOREIGN KEY (`konfirmasi_trans_no`) REFERENCES `tb_transaksi` (`transaksi_nomor`),
@@ -427,7 +450,7 @@ ALTER TABLE `tb_konfirmasi`
   ADD CONSTRAINT `tb_konfirmasi_ibfk_3` FOREIGN KEY (`konfirmasi_admin_id`) REFERENCES `tb_admins` (`admin_id`);
 
 --
--- Ketidakleluasaan untuk tabel `tb_stok`
+-- Constraints for table `tb_stok`
 --
 ALTER TABLE `tb_stok`
   ADD CONSTRAINT `tb_stok_ibfk_1` FOREIGN KEY (`stok_barang_id`) REFERENCES `tb_barang` (`barang_id`) ON DELETE CASCADE ON UPDATE CASCADE;
