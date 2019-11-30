7a6f0d64fc5e9f224c9c71e5ccd97f9a769b07f0
alfanizidni <48055492+alfanizidni@users.noreply.github.com>
2019-11-30 04:56:25 -0800

    laporan transaksi masuk

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LaporanController extends Controller
 {
     //
     public function lapMasuk() {
         
+
+        $lap_masuk = DB::table('tb_detail_transaksi_masuk')
+            ->join('tb_stok', 'tb_stok.stok_id', '=', 'tb_detail_transaksi_masuk.detiltrans_masuk_idstok')
+            ->join('tb_transaksi_masuk', 'tb_detail_transaksi_masuk.detiltrans_masuk_idtrans', '=', 'tb_transaksi_masuk.trans_masuk_id')
+            ->groupBy('trans_masuk_id')
+            ->get();
+
         $data = [
             'title' => "Laporan",
-            'breadcrumb' => "Laporan Barang Masuk"
+            'breadcrumb' => "Laporan Barang Masuk",
+            'laporan_masuk' => $lap_masuk
         ];
         
         return view ('admin/lap_barangMasuk', $data);
--- resources/views/admin/lap_barangMasuk.blade.php
+++ resources/views/admin/lap_barangMasuk.blade.php
@@ -1,1 +1,55 @@
 @extends('admIndex')
+
+@section('content')
+<!-- DataTables example -->
+<div class="row">
+    <div class="col s12 m12 l12">
+        <div id="button-trigger" class="card card card-default scrollspy">
+            <div class="card-content">
+                <div class="col s12 m6 l7">
+                    <h4 class="card-title">Laporan Transaksi Masuk</h4>
+                </div>
+                <div class="row">
+                    <div class="col s12">
+                        <table id="data-table-simple" class="display">
+                            <thead>
+                                <tr style="background-color: orange">
+                                    <th>Nomor Transaksi</th>
+                                    <th>Tanggal Transaksi</th>
+                                    <th>Suplier/Toko</th>
+                                    <th>Total Harga</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if(!empty($lap_masuk))
+                                @foreach($lap_masuk as $lap)
+                                <tr>
+                                    <td>{{ $lap->trans_masuk_id }}</td>
+                                    <td>{{ $lap->trans_masuk_tanggal }}</td>
+                                    <td>{{ $lap->trans_masuk_suplier }}</td>
+                                    <td>{{ $lap->trans_masuk_totalharga }}</td>
+                                </tr>
+                                <!-- Modal Icons -->
+                                <div id="modaldelete{{ $brg->barang_id }}" class="modal">
+                                    <div class="modal-content">
+                                        <h5>Apakah Anda Yakin Untuk Menghapus Data Ini?</h5>
+                                        <hr>
+                                        <h4 class="mt-5"> &ensp; {{ $brg->barang_nama }}</h4>
+                                        <h6> &ensp; Data Ini Tidak Dapat Dikembalikan Setelah Proses Hapus, Apakah Anda Yakin?</h6>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <a href="/admin/delete-grafik/{{ $brg->barang_id }}" class="modal-action modal-close waves-effect waves-light red accent-2 btn-flat" style="color: white">Delete</a>
+                                    </div>
+                                </div>
+                                @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
