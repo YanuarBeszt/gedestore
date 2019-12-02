0058da5209211381d8dc7e0a9e6bddf124ac9896
alfanizidni <48055492+alfanizidni@users.noreply.github.com>
2019-12-02 04:50:28 -0800

    Laporan Barang Keluar

--- resources/views/admin/lap_barangKeluar.blade.php
+++ resources/views/admin/lap_barangKeluar.blade.php
@@ -1,1 +1,43 @@
 @extends('admIndex')
+
+@section('content')
+<!-- DataTables example -->
+<div class="row">
+    <div class="col s12 m12 l12">
+        <div id="button-trigger" class="card card card-default scrollspy">
+            <div class="card-content">
+                <div class="col s12 m6 l7">
+                    <h4 class="card-title">Laporan Transaksi Keluar</h4>
+                </div>
+                <div class="row">
+                    <div class="col s12">
+                        <table id="data-table-simple" class="display">
+                            <thead>
+                                <tr style="background-color: orange">
+                                    <th>Nomor Transaksi</th>
+                                    <th>Tanggal Transaksi</th>
+                                    <th>Jumlah Barang</th>
+                                    <th>Total Harga</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if(!empty($laporan_keluar))
+                                @foreach($laporan_keluar as $lap)
+                                <tr>
+                                    <td>{{ $lap->transaksi_nomor }}</td>
+                                    <td>{{ $lap->transaksi_tanggal }}</td>
+                                    <td>{{ $lap->total_sales }}</td>
+                                    <td>{{ $lap->total }}</td>
+                                </tr>
+                                @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
